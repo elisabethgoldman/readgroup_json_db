d0937616ec3c8332dd80c21d018154589c960c47
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-08-11 18:52:17 -0500

    cleanup

--- readgroup_json_db/main.py
+++ readgroup_json_db/main.py
@@ -8,10 +8,6 @@ import sys
 import pandas as pd
 import sqlalchemy
 
-from cdis_pipe_utils import pipe_util
-from cdis_pipe_utils import df_util
-
-
 def readgroup_to_db(json_data, uuid, engine, logger):
     table_name = 'readgroups'
     for rg_key in sorted(json_data.keys()):
@@ -21,9 +17,22 @@ def readgroup_to_db(json_data, uuid, engine, logger):
         rg_dict['key'] = rg_key
         rg_dict['value'] = json_data[rg_key]
         df = pd.DataFrame(rg_dict)
-        unique_key_dict = {'uuid': uuid, 'ID': json_data['ID'], 'key': rg_key}
-        df_util.save_df_to_sqlalchemy(df, unique_key_dict, table_name, engine, logger)
+        df.to_sql(table_name, engine)
     return
+
+
+def setup_logging(args, uuid):
+    basicConfig(
+        filename=os.path.join(uuid + '.log'),
+        level=args.level,
+        filemode='w',
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt='%Y-%m-%d_%H:%M:%S_%Z',
+    )
+    getLogger('sqlalchemy.engine').setLevel(INFO)
+    logger = getLogger(__name__)
+    return logger
+
 
 def main():
     parser = argparse.ArgumentParser('readgroup json db insertion')
@@ -39,21 +48,18 @@ def main():
     
     # Required flags.
     parser.add_argument('--json_path',
-                        required = True,
-                        help = 'readgroup json path'
+                        required = True
     )
     parser.add_argument('--uuid',
-                        required = True,
-                        help = 'uuid string',
+                        required = True
     )
 
     # setup required parameters
     args = parser.parse_args()
-    tool_name = 'readgroup_json_db'
     uuid = args.uuid
     json_path = args.json_path
 
-    logger = pipe_util.setup_logging(tool_name, args, uuid)
+    logger = setup_logging(args, uuid)
 
     sqlite_name = uuid + '.db'
     engine_path = 'sqlite:///' + sqlite_name
@@ -61,7 +67,7 @@ def main():
 
     with open(json_path, 'r') as json_open:
         json_data = json.load(json_open)
-    readgroup_to_db(json_data, uuid, engine, logger)
+        readgroup_to_db(json_data, uuid, engine, logger)
         
     return
 
