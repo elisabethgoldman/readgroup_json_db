12fba1483673868ef1f2fc2ae2f12a9cd0356c21
Charles Czysz <czysz@uchicago.edu>
2022-01-26 14:54:02 -0600

    Python files updated

--- setup.py
+++ setup.py
@@ -1,28 +1,10 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name = 'readgroup_json_db',
-      author = 'Jeremiah H. Savage',
-      author_email = 'jeremiahsavage@gmail.com',
-      version = 0.27,
-      description = 'read readgroup data as json and store in sqlitedb',
-      url = 'https://github.com/jeremiahsavage/readgroup_json_db',
-      license = 'Apache 2.0',
-      packages = find_packages(),
-      install_requires = [
-          'pandas',
-          'sqlalchemy'
-      ],
-      classifiers = [
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Apache Software License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-      ],
-      entry_points={
-          'console_scripts': ['readgroup_json_db=readgroup_json_db.__main__:main']
-      },
+setup(
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 )
+
+# __END__
